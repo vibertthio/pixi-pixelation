0faf122607c1545f290bf056f4e341813f55f9ab
Vibert Thio <vibertthio@gmail.com>
2019-05-14 11:02:24 +0800

    :fire: remove folder public

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      { from: Path.resolve(__dirname, '../public'), to: 'public' }
+      // { from: Path.resolve(__dirname, '../public'), to: 'public' }
     ]),
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/template.html')
