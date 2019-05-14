f2bd25572777945c7d6e299ffaa7bd35a7e12fa0
Vibert Thio <vibertthio@gmail.com>
2019-05-14 12:14:29 +0800

    :boy: add basic image import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-import './index.scss';
 import * as PIXI from 'pixi.js';
 
+import './index.scss';
+import bunnyImg from './assets/bunny.png';
 
-const app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
+// setup
+const app = new PIXI.Application({
+  width: 400,
+  height: 400,
+  backgroundColor: 0x373a6d,
+  transparent: false,
+});
 document.body.appendChild(app.view);
 
+// check WebGL support
+console.log(PIXI.utils.isWebGLSupported());
+console.log(PIXI.RENDERER_TYPE);
+console.log(app.renderer.type);
+
+// use image
+const bunny = PIXI.Sprite.from(bunnyImg);
+bunny.anchor.set(0.5);
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+app.stage.addChild(bunny);
+
+// animation
+app.ticker.add((delta) => {
+  bunny.rotation += 0.1 * delta;
+});
