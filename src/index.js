583e6b5ae7c2919a70104b4bed0323e1ea6f7a6f
Vibert Thio <vibertthio@gmail.com>
2019-05-14 11:59:58 +0800

    :boy: add basic canvas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './index.scss';
+import * as PIXI from 'pixi.js';
 
-console.log('webpack starterkit');
+
+const app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
+document.body.appendChild(app.view);
+
