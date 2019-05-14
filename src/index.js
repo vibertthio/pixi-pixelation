25d0f5a3f2e63f601c72cdd370aeb8cf46119077
Vibert Thio <vibertthio@gmail.com>
2019-05-14 16:16:55 +0800

    :sushi: add TilingSprite

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as PIXI from 'pixi.js';
 
 import './index.scss';
 import bunnyImg from './assets/bunny.png';
+import venusImg from './assets/venus_1000.jpg';
 
 // setup
 const app = new PIXI.Application({
@@ -23,6 +24,26 @@ bunny.anchor.set(0.5);
 bunny.x = app.screen.width / 2;
 bunny.y = app.screen.height / 2;
 app.stage.addChild(bunny);
+
+// texture
+const venusTexture = PIXI.Texture.from(venusImg);
+const { baseTexture, frame } = venusTexture;
+const crop = new PIXI.Rectangle(
+  venusTexture.frame.x,
+  venusTexture.frame.y,
+  venusTexture.frame.width * 0.5,
+  venusTexture.frame.height * 0.5,
+);
+const trim = new PIXI.Rectangle(
+  venusTexture.frame.x + 100,
+  venusTexture.frame.y + 100,
+  venusTexture.frame.width * 0.3,
+  venusTexture.frame.height * 0.3,
+);
+const rotatedTexture = new PIXI.Texture(baseTexture, frame, crop, trim, 0);
+const venusSprite = new PIXI.TilingSprite(rotatedTexture, window.innerWidth, window.innerHeight);
+venusSprite.anchor.set(0);
+app.stage.addChild(venusSprite);
 
 // animation
 app.ticker.add((delta) => {
